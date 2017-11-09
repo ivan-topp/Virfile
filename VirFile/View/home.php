d94752a8d6961e9a9b3996004e606e00e246fe45
Iván Topp Sandoval <itopp2015@alu.uct.cl>
2017-11-08 22:50:29 -0300

    Actualización de Vista Principal.
    Se actualiza la vista principal "home" y se agrega Bootstrap y jquery en respectivos directorios.

--- VirFile/View/home.php
+++ VirFile/View/home.php
@@ -2,9 +2,23 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./Resources/Bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="./resources/App/css/styles.css">
 	<title>Perico</title>
 </head>
 <body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12 service">
+				<div class="center">
+					<h1>¿Qué Ofrecemos?</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur officia expedita beatae veniam alias adipisci magni eius dolores vero dolore id rerum necessitatibus, modi quasi nobis esse placeat officiis, quo?</p>
+				</div>
+			</div>
+		</div>
+	</div>
 	<form action="" method="POST">
 		<label for="User"></label>
 		<input type="text" name="User" placeholder="Usuario o Correo" required>
@@ -12,5 +26,7 @@
 		<input type="password" name="Pass" placeholder="Contraseña" required>
 		<input type="submit" name="Submit">
 	</form>
+	<script src="./Resources/Jquery/jquery-3.2.1.min.js">"></script>
+	<script src="./Resources/Bootstrap/js/bootstrap.min.js">"></script>
 </body>
 </html>
