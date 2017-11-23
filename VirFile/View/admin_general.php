3d14cd6d3c06c6b944f9beb81780231c8d964325
Cristobal <cdiaz2016@alu.uct.cl>
2017-11-23 16:53:21 -0300

    arreglo

--- VirFile/View/admin_general.php
+++ VirFile/View/admin_general.php
@@ -1,5 +1,7 @@
 <?php include('header.php');?>
 	<title>VirFile</title>
+	<link rel="stylesheet" href="../Resources/Bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../resources/App/css/styles.css">
 </head>
 <body>
 	<div class="container-fluid">
@@ -17,28 +19,30 @@
 				</div>
 			</div>
 			<div class="col-xs-3" style="padding-left: 5vh;padding-right: 5vh; height: 100vh; background-color: #8181F7">
-				<div class="row" style="background-color: #8181F7;height: 20vh; padding-left: 5vw; padding-right: 1vw;padding-top: 1vw; font-family: Century Gothic, Georgia; -webkit-user-select: none; -moz-user-select: none;-khtml-user-select: none;-ms-user-select:none;">
+				<div class="row" style="background-color: #8181F7;height: 20vh auto; padding-left: 40%; padding-right: 50%;padding-top: 1%; font-family: Century Gothic, Georgia; -webkit-user-select: none; -moz-user-select: none;-khtml-user-select: none;-ms-user-select:none;">
 					<div style="text-align: right;">
 						<input type="submit" value="LogOut">
 					</div>
 					<br>
 					<img src="../Resources/Image/icono_standar.png" alt="c" height="50"/>
-					Name_user.perico
+					<br>
+					<div style="margin-left: 1%; margin-right: 1%;">Name_user.perico</div>
+					
 				</div>
-				<br>
+					<br>
 				<div class="row" style="background-color: #A4A4A0;height: 7.5vh; padding-left: 0vw; padding-top: 1.1vw; font-family: Century Gothic, Georgia; border-style: outset; -webkit-user-select: none; -moz-user-select: none;-khtml-user-select: none;-ms-user-select:none; ">
 					<div style="text-align: center;">Manage Enterprise</div>
 				</div>
 				<br>
-				<div class="row" style="background-color: #A4A400;height: 7.5vh; padding-left: 0vw; padding-top: 1.1vw; font-family: Century Gothic, Georgia; border-style: outset; -webkit-user-select: none; -moz-user-select: none;-khtml-user-select: none;-ms-user-select:none;">
+				<div class="row" style="background-color: #A4A400;height: 7.5vh; padding-left: 1%; padding-top: 1.1%;padding-right:1%; font-family: Century Gothic, Georgia; border-style: outset; -webkit-user-select: none; -moz-user-select: none;-khtml-user-select: none;-ms-user-select:none;">
 					<div style="text-align: center;">Register Enterprice</div>
 				</div>
 				<br>
-				<div class="row" style="background-color: #A4A4FF;height: 7.5vh; padding-left: 0vw; padding-top: 1.1vw; font-family: Century Gothic, Georgia; border-style: outset; -webkit-user-select: none; -moz-user-select: none;-khtml-user-select: none;-ms-user-select:none;">
+				<div class="row" style="background-color: #A4A4FF;height: 7.5vh; padding-left: 1%; padding-top: 1.1%;padding-right:1%; font-family: Century Gothic, Georgia; border-style: outset; -webkit-user-select: none; -moz-user-select: none;-khtml-user-select: none;-ms-user-select:none;">
 					<div style="text-align: center;">Register User</div>
 				</div>
 				<br>
-				<div class="row" style="background-color: #A4A4AA;height: 7.5vh; padding-left: 0vw; padding-top: 1.1vw; font-family: Century Gothic, Georgia; border-style: outset; -webkit-user-select: none; -moz-user-select: none;-khtml-user-select: none;-ms-user-select:none;">
+				<div class="row" style="background-color: #A4A4AA;height: 7.5vh; padding-left: 0%; padding-top: 1%;padding-right:1%; font-family: Century Gothic, Georgia; border-style: outset; -webkit-user-select: none; -moz-user-select: none;-khtml-user-select: none;-ms-user-select:none;" id="Setting" name="Setting" onclick="">
 					<div style="text-align: center;">Setting</div>
 				</div>
 				<br>
@@ -50,6 +54,6 @@
 	</div>
 	<script src="./Resources/Jquery/jquery-3.2.1.min.js"></script>
 	<script src="./Resources/Bootstrap/js/bootstrap.min.js"></script>
-	<script src="./Resources/App/js/Main.js"></script>
+	<script src="./Resources/App/js/GeneralActions.js"></script>
 </body>
 </html>
