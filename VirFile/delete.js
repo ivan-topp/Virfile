e7ed5baf822662258c1c40e46733fc53baba7c96
hanstopp <htopp2016@alu.uct.cl>
2017-11-27 13:01:05 -0300

    JS Delete

---
+++ VirFile/delete.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("btndelete").click(function(event){
+    	event.preventDefault();
+		var Datos = { Action : "delete"};
+		$.post("./recibe.php",
+			Datos,
+	    	function(data, status){
+	    		if(status != "success"){console.log("Error al realizar la peticion.");}
+	    		else{
+	    			console.log("Data: " + data);
+	    			//location.reload();
+	    		}
+	        	
+	    });
+	});
+});
