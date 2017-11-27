00b42ee20b5d4b6ea0f5caedff4d2bf188810d36
hanstopp <htopp2016@alu.uct.cl>
2017-11-27 13:02:54 -0300

    Funcion UserList ( ) y UserDelte ( )

--- VirFile/Model/adminModel.php
+++ VirFile/Model/adminModel.php
@@ -1,8 +1,46 @@
 <?php
 	require_once('./Db/db.php');
 	class adminModel{
-		function __contruct($controller){
+
+		private $sql  = "SELECT * FROM USER WHERE Enterprise = :ep;";
+		private $sql1 = "DELETE * FROM USER WHERE ID_User = :us;";
+		//SELECT * FROM `user` WHERE Enterprise="Empresa" AND User_Level=1
+		private $db;
+
+		function __construct($controller){
 			$this->controller = $controller;
+		}
+
+		function User_List(){
+			$this->db = Database::Connect();
+			//function login_access($User,$Pass){
+
+			$query = $this -> db -> prepare($this -> sql);
+			$query->execute(array(':ep'=>$_SESSION['Enterprise']));
+			$rows=$query->fetchAll();//(PDO::FETCHALL);
+			if($query->rowCount()>0){
+				return $rows;
+			}
+			else{
+				return False;
+			}
+			
+		}
+
+		function User_Delete(){
+			$this->db = Database::Connect();
+			//function login_access($User,$Pass){
+
+			$query = $this -> db -> prepare($this -> sql1);
+			$query->execute(array(':us'=>$_SESSION['ID']));
+			$rows=$query->fetchAll();//(PDO::FETCHALL);
+			if($query->rowCount()>0){
+				return $rows;
+			}
+			else{
+				return False;
+			}
+			
 		}
 	}
 ?>
