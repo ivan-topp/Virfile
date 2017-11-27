77762fa7ba8bd91c1ec5233571919407dc6cbcb0
hanstopp <htopp2016@alu.uct.cl>
2017-11-27 13:02:07 -0300

    Funcion Listar() y Eliminar()

--- VirFile/Controller/adminController.php
+++ VirFile/Controller/adminController.php
@@ -1,16 +1,23 @@
 <?php
 	require_once('./Model/adminModel.php');
 	class adminController{
-		function __contruct($id, $uname, $company, $stock, $name, $mail, $pass, $level){
-			$this->id      = $id;
+		function __construct(/*$id, $uname, $company, $stock, $name, $mail, $pass, $level*/){
+			/*$this->id      = $id;
 			$this->uname   = $uname;
 			$this->company = $company;
 			$this->stock   = $stock;
 			$this->name    = $name;
 			$this->mail    = $mail;
 			$this->pass    = $pass;
-			$this->level   = $level;
+			$this->level   = $level;*/
 			$this->Model   = new adminModel($this);
+		}
+
+		function Listar(){
+			return $this->Model->User_List();
+		}
+		function Eliminar(){
+			return $this->Model->User_Delete();
 		}
 	}
 ?>
