1df7d84f78fe69de3157148b92a681e1f19fc3f9
Iván Topp Sandoval <itopp2015@alu.uct.cl>
2017-11-29 23:26:30 -0300

    Validación de modificar.
    se valida para que al modificar solo introdusca el datoa  modificar.

--- VirFile/Model/adminModel.php
+++ VirFile/Model/adminModel.php
@@ -4,24 +4,51 @@
 	class adminModel extends userModel{
 		private $sqlUpdate = "UPDATE user SET Stock = :Stock WHERE ID_User = :User"; //Maximo = 209715200 Bytes
 		private $sqlGetStock = "SELECT Stock FROM user WHERE ID_User = :User LIMIT 1";
-		private $sql =  "SELECT user.ID_User, user.User_Name, user.Mail
+		//private $sql  = "SELECT * FROM USER WHERE Enterprise = :ep;";
+
+		private $sql =  "SELECT user.ID_User, user.User_Name, user.Mail, user.Name, user.Stock, user.User_Level, enterprise.Name AS empresa
 					    FROM Enterprise
 						INNER JOIN user
 						on enterprise.ID_E = user.ID_E
-						WHERE user.ID_E = :ep; AND user.ID_User!=:user";
-		private $sqlUpdate2 = "UPDATE user SET User_Name = :nus, Name=:nm, Mail=:mail, Password =:psw, User_Level =:uslv WHERE ID_User = :User";
+						WHERE user.ID_E = :ep AND user.ID_User != :user";
+
+		
+		private $sqlUpdate2 = "UPDATE user SET ";//User_Name = :nus, Name=:nm, Mail=:mail, Password =:psw, User_Level =:uslv WHERE ID_User = :User";
+
+
 		private $sql1 = "DELETE FROM USER WHERE ID_User = :us;";
 		private $db;
-
 		function __construct(){
 			parent::__construct();
 			$this->db = Database::Connect();
 		}
 
-		function Edit($id,$uN,$N,$M,$Psw,$Lvl){
-			$this->db = Database::Connect();
+		function Edit($id, $uN = '', $N = '', $M = '', $Psw = '', $Lvl = ''){
+			$values = array();
+			if($uN != ''){
+				$this->sqlUpdate2 = $this->sqlUpdate2 . 'User_Name = :nus ';
+				$values[':nus'] = $uN;
+			}
+			if($N != ''){
+				$this->sqlUpdate2 = $this->sqlUpdate2 . 'Name=:nm ';
+				$values[':nm'] = $N;
+			}
+			if($M != ''){
+				$this->sqlUpdate2 = $this->sqlUpdate2 . 'Mail=:mail ';
+				$values[':mail'] = $M;
+			}
+			if($Psw != ''){
+				$this->sqlUpdate2 = $this->sqlUpdate2 . 'Password =:psw ';
+				$values[':psw'] = $Psw;
+			}
+			if($Lvl != ''){
+				$this->sqlUpdate2 = $this->sqlUpdate2 . 'User_Level =:uslv ';
+				$values[':uslv'] = intval($Lvl);
+			}
+			$this->sqlUpdate2 = $this->sqlUpdate2.'WHERE ID_User = :User';
+			$values[':User']=$id;
 			$query = $this -> db -> prepare($this -> sqlUpdate2);
-			if($query->execute(array(':nus'=>$uN, ':nm'=>$N, ':mail'=>$M, ':psw'=>$Psw, ':uslv'=>$Lvl, ':User'=>$id)))
+			if($query->execute($values))
 			{
 				return True;
 			}
@@ -31,9 +58,11 @@
 		}
 
 		function User_List(){
+			
+			//function login_access($User,$Pass){
 			$query = $this -> db -> prepare($this -> sql);
-			$query->execute(array(':user'=>$_SESSION['ID'],':ep'=>$_SESSION['Enterprise']));
-			$rows=$query->fetchAll();
+			$query->execute(array(':ep'=>$_SESSION['Enterprise'], ':user'=>$_SESSION['ID']));
+			$rows=$query->fetchAll();//(PDO::FETCHALL);
 			if($query->rowCount()>0){
 				return $rows;
 			}
@@ -52,5 +81,8 @@
 				return False;
 			}	
 		}
+
+		
+
 	}
 ?>
--- VirFile/Resources/App/js/action.js
+++ VirFile/Resources/App/js/action.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
 	    			$('.User').remove();
 	    			console.log("Data: " + data);
 	    			data = JSON.parse(data);
-	    			$('#ListUsers').append('<tr class="User"><td>ELIMINAR</td><td>ID USUARIO </td><td>NOMBRE USUARIO</td><td>CORREO</td></tr>');
+	    			$('#ListUsers').append('<tr class="User"><td>ELIMINAR</td><td>ID USUARIO </td><td>NOMBRE USUARIO</td><td>NOMBRE</td><td>CORREO</td><td>Empresa</td><td>Nivel</td></tr>');
 	    			data.forEach(function(val, indx){
-	    				$('#ListUsers').append('<tr class="User"><td><input type="checkbox" id="'+val.ID_User+'" name="numero[]"></td><td>'+val.ID_User+'</td><td>'+val.User_Name+'</td><td>'+val.Mail+'</td></tr>');
+	    				$('#ListUsers').append('<tr class="User"><td><input type="checkbox" id="'+val.ID_User+'" name="numero[]"></td><td>'+val.ID_User+'</td><td>'+val.User_Name+'</td><td>'+val.Name+'</td><td>'+val.Mail+'</td><td>'+val.empresa+'</td><td>'+val.User_Level+'</td></tr>');
 	    				console.log(val);
 	    			});
 	    		}
@@ -80,33 +80,29 @@ $(document).ready(function() {
 
 		});	
 	});*/
-
+	$('#btnedit').click(function(){
+		if($("input:checkbox:checked").length != 1) $('#_status2').html('<div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Error!</strong> Selecciona solo un usuario.</div>');
+		else $('#_status2').html('');
+	});
 
 	$('#editUser').click(function(){
-		$("input:checkbox:checked").each(function() {
-			//console.log(parseInt($(this).attr('id')));
-			var id = $(this).attr('id');	
-			var switchs= parseInt($('#editUserLevel').val());
-			//console.log(switchs);
-			if(switchs==0 || switchs==1){
-				
-				var Datos = { Action : "Edit", ID : parseInt(id) , UserName : $('#editUserUserName').val(), Name : $('#editUserName').val(), Mail : $('#editUserMail').val(), Pass : $('#editUserPass').val(), Level : parseInt($('#editUserLevel').val())};
-				console.log(Datos);
+		if($("input:checkbox:checked").length == 1){
+			$("input:checkbox:checked").each(function() {
+				var id = $(this).attr('id');
+				var Datos = { Action : "Edit", ID : parseInt(id) , UserName : $('#editUserUserName').val(), Name : $('#editUserName').val(), Mail : $('#editUserMail').val(), Pass : $('#editUserPass').val(), Level : $('#editUserLevel').val()};
 				$.post("./Front.php",
 					Datos,
-		    		function(data, status){
-		    			if(status != "success"){console.log("Error al realizar la peticion.");}
-		    			else{
-		    				console.log("HOLA");
+			   		function(data, status){
+			   			if(status != "success"){console.log("Error al realizar la peticion.");}
+			   			else{
 			    			data = JSON.parse(data);
 			    			if(Object.keys(data).indexOf('Error') == -1){
-			    				console.log(data);
-			    				//location.reload();//location.href='./user.php';
-			    			}else $('#_status').html('<div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Error!</strong> Los datos ingresados no son correctos, asegurate de haber ingresado bien tus datos.</div>');
+			    				Listar();
+			    			}else $('#_status2').html('<div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Error!</strong> Error 1.</div>');
 			    		}
-		    		});
-		    }else $('#_status').html('<div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Error!</strong> Los datos ingresados no son correctos, asegurate de haber ingresado bien tus datos.</div>'); 
-		});	
+			   		});
+			});
+		}else $('#_status2').html('<div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Error!</strong> Selecciona solo un usuario.</div>');
 	});
 	$('#registerUser').click(function(){
 		
